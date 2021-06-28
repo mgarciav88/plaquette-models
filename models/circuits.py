b6de6bdaacd9c634b777ae05fa580ca47c36dae2
mgarciav88 <miguelgarciavera88@gmail.com>
2021-06-28 09:00:07 +0100

    Added support for square U(1) circuit

--- models/circuits.py
+++ models/circuits.py
@@ -1,3 +1,5 @@
+import itertools
+
 import numpy as np
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 
@@ -51,6 +53,23 @@ class Plaquette:
             self.circuit.sdg(self.q_register[q_ind])
             self.circuit.h(self.q_register[q_ind])
 
+    def x_rotate(self, q_list):
+        for q_ind in q_list:
+            self.circuit.h(self.q_register[q_ind])
+
+    def x_back_rotate(self, q_list):
+        self.x_rotate(q_list)
+
+    def y_back_rotate(self, q_list):
+        for q_ind in q_list:
+            self.circuit.sdg(self.q_register[q_ind])
+            self.circuit.h(self.q_register[q_ind])
+
+    def y_rotate(self, q_list):
+        for q_ind in q_list:
+            self.circuit.h(self.q_register[q_ind])
+            self.circuit.s(self.q_register[q_ind])
+
     def time_evolution(self, q_control, time_factor=1):
         self.circuit.h(self.q_register[q_control])
         self.circuit.u1(2 * time_factor * self.t * self.g, self.q_register[q_control])
@@ -86,6 +105,8 @@ class SinglePlaquette(Plaquette):
     def u1_models(self, q_control, qs_real):
         if self.n_qubits - 1 == 3:
             self.generate_triangle_u1(q_control, qs_real)
+        elif self.n_qubits - 1 == 4:
+            self.generate_square_u1(q_control, qs_real)
 
         c_register = ClassicalRegister(self.n_qubits, 'c')
 
@@ -107,15 +128,61 @@ class SinglePlaquette(Plaquette):
         self.apply_h_gate(qs_real)
 
         for q_ind in qs_real[::-1]:
-            self.backward_rotate_qubits(q_ind, qs_real)
+            self.x_back_rotate([q_ind])
+            qs_real_copy = qs_real.copy()
+            qs_real_copy.remove(q_ind)
+            self.y_back_rotate(qs_real_copy)
+            # self.backward_rotate_qubits(q_ind, qs_real)
 
             self.forward_entangle(qs_real, q_control)
             self.time_evolution(q_control, time_factor=-1)
             self.backward_entangle(qs_real, q_control)
 
-            self.rotate_qubits(q_ind, qs_real)
+            self.y_rotate(qs_real_copy)
+            self.x_rotate([q_ind])
+
+            # self.rotate_qubits(q_ind, qs_real)
 
         self.circuit.u2(-np.pi / 2, -np.pi / 2, self.q_register[q_control])
+
+    def generate_square_u1(self, q_control, qs_real):
+        self.circuit.h(self.q_register[q_control])
+
+        self.apply_h_gate(qs_real)
+
+        self.forward_entangle(qs_real, q_control)
+        self.time_evolution(q_control)
+        self.backward_entangle(qs_real, q_control)
+
+        self.apply_h_gate(qs_real)
+
+        self.y_back_rotate(qs_real)
+
+        self.forward_entangle(qs_real, q_control)
+        self.time_evolution(q_control)
+        self.backward_entangle(qs_real, q_control)
+
+        self.y_rotate(qs_real)
+
+        for x_q_pair in itertools.combinations(qs_real, 2):
+            y_q_pair = [q_ind for q_ind in qs_real if q_ind not in x_q_pair]
+            self.x_back_rotate(x_q_pair)
+            self.y_back_rotate(y_q_pair)
+
+            if (x_q_pair[0] in qs_real[0:1] and x_q_pair[1] in qs_real[0:1]) or (
+                    x_q_pair[0] in qs_real[2:3] and x_q_pair[1] in qs_real[2:3]):
+                time_factor = -1
+            else:
+                time_factor = 1
+
+            self.forward_entangle(qs_real, q_control)
+            self.time_evolution(q_control, time_factor=time_factor)
+            self.backward_entangle(qs_real, q_control)
+
+            self.y_rotate(y_q_pair)
+            self.x_rotate(x_q_pair)
+
+        self.circuit.h(self.q_register[q_control])
 
     def generate_square_z2(self, q_control, qs_real):
         self.circuit.h(self.q_register[q_control])
